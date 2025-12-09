e21f80326c3336fe736115848a6e6f8a19ea1da5
Maciej Grabowski <148635267+mlodykizownik58@users.noreply.github.com>
2025-12-09 13:39:47 +0100

    Add files via upload

---
+++ Services/Roleinitializer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EventManagement.Services
+{
+    public class RoleInitializer
+    {
+        public static async Task SeedRolesAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            // Lista ról do dodania
+            string[] roleNames = { "Organizer", "Participant" };
+
+            foreach (var roleName in roleNames)
+            {
+                if (!await roleManager.RoleExistsAsync(roleName))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+        }
+    }
+}
